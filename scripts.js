b2cbce922edfe134c2d24407bcb6717abb906286
Casie Siekman <casie.siekman@gmail.com>
2020-05-12 11:14:30 -0500

    udpates

---
+++ scripts.js
@@ -0,0 +1,56 @@
+console.log('1' + '12');
+console.log('string plus a number', ('12' + 1));
+
+console.log(1 + 12);
+
+console.log('12' - '1' );
+console.log('12' * '2');
+
+console.log('12' / '2');
+
+console.log('twelve' / '2');
+
+
+function addinator(numberOne, numberTwo){
+    console.log(numberTwo, numberOne);
+    return numberOne / numberTwo;
+}
+
+
+// addinator('twelve', '2');
+
+// null
+
+// undefined
+
+// not defined 
+
+// if else
+
+// if(value1 > value2){
+//     // do this
+// } else if (value1 === value2){
+//     // do this
+// } else {
+//     // do this
+// }
+
+
+
+function doubleIt(x){
+    return x * 2;
+};
+
+function halfIt(){
+   console.log(x / 2);
+};
+
+console.log(doubleIt(3));
+
+console.log(halfIt(12));
+
+
+
+
+
+
